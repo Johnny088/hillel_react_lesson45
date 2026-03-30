f718c3e44eb38c536169c753855b64c89b2b1481
Johnny <esp25goodlife@gmail.com>
2026-03-30 07:31:22 +0200

    add navigation to the main page

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,3 +1,12 @@
+import css from './mainPage.module.css';
+import { useNavigate } from 'react-router';
+
 export const MainPage = () => {
-  return <h1>Main Page</h1>;
+  const navigate = useNavigate();
+  return (
+    <div className={css.container}>
+      <h1>Welcome to the website</h1>
+      <button onClick={() => navigate('/products')}>show products</button>
+    </div>
+  );
 };
